b9c316f9734b36c480416547adf7fd1481fa5d25
Samuel Akinboro <victorakinboro2003@gmail.com>
2022-05-26 05:02:35 +0100

    finished counter

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -2,15 +2,35 @@ import React from 'react'
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
 
 const CounterScreen = ({ navigation }) => {
+  const [value, setValue] = React.useState(0)
+
+  const increment = () => {
+    setValue(prevState => prevState + 1)
+  }
+
+  const decrement = () => {
+    if(value > 0) {
+      setValue(prevState => prevState - 1)
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>0</Text>
-      <TouchableOpacity>
-        <Text style={styles.buttonText}>-</Text>
-      </TouchableOpacity>
-      <TouchableOpacity>
-        <Text style={styles.buttonText}>+</Text>
-      </TouchableOpacity>
+      <Text style={styles.text}>{value}</Text>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity 
+          style={styles.button}
+          onPress={() => decrement()}
+        >
+          <Text style={styles.buttonText}>-</Text>
+        </TouchableOpacity>
+        <TouchableOpacity 
+          style={styles.button}
+          onPress={() => increment()}
+        >
+          <Text style={styles.buttonText}>+</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   )
 }
@@ -23,13 +43,25 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   text: {
-    fontSize: 60
+    fontSize: 160
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    width: 180,
+    justifyContent: 'space-between',
+    marginTop: 100
+  },
+  button: {
+    height: 70,
+    width: 70,
+    backgroundColor: "orange",
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 40
   },
   buttonText: {
-    fontSize: 20,
-    height: 60,
-    width: 60,
-    backgroundColor: "orange"
+    fontSize: 30,
+    color: 'white'
   }
 })
 
