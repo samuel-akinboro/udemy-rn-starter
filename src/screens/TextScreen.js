09a71d64f78e07bb1f48bfecc73ca183d60493e0
Samuel Akinboro <victorakinboro2003@gmail.com>
2022-05-27 13:03:37 +0100

    Text screen completion

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -2,14 +2,34 @@ import { StyleSheet, Text, View, TextInput } from 'react-native'
 import React from 'react'
 
 const TextScreen = () => {
+  const [name, setName] = React.useState('');
+  const [password, setPassword] = React.useState('')
+
   return (
     <View style={styles.container}>
-      <Text>Enter Name Below</Text>
+      <Text>Enter Name</Text>
       <TextInput 
         style={styles.textInput} 
         autoCorrect={false}
         autoCapitalize="none"
+        value={name}
+        onChangeText={(inputValue) => setName(inputValue)}
       />
+      <Text style={{marginTop: 10}}>Enter Passowrd</Text>
+      <TextInput 
+        style={styles.textInput} 
+        autoCorrect={false}
+        autoCapitalize="none"
+        value={password}
+        onChangeText={(inputValue) => setPassword(inputValue)}
+      />
+      {(password.length > 0 && password.length < 5) &&
+          <Text>Passoword should be at least 5 characters </Text>
+        }
+      <Text style={styles.output}>
+        My Name is
+        <Text style={styles.highlight}> {name}</Text>
+      </Text>
     </View>
   )
 }
@@ -26,5 +46,12 @@ const styles = StyleSheet.create({
     borderRadius: 6,
     padding: 5,
     marginTop: 5
+  },
+  output: {
+    fontSize: 40,
+    marginTop: 20
+  },
+  highlight: {
+    color: 'orange'
   }
 })
